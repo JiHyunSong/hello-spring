45ee3fe6ccb3a5012c4b912fbf052dafd7fd284b
JiHyunSong <jihyun.song0829@gmail.com>
2018-10-28 15:59:38 +0900

    dead

--- src/com/jenny/MainApplication.java
+++ src/com/jenny/MainApplication.java
@@ -14,24 +14,24 @@ import java.util.Arrays;
 public class MainApplication {
 
     public static void main(String[] args) {
-        System.out.println("hmm1");
-//        ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
+
+        System.out.println("a");
         SpringApplication.run(MainApplication.class, args);
-        System.out.println("hmm2");
+        System.out.println("b");
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
 }
--- src/com/jenny/controller/HealthController.java
+++ src/com/jenny/controller/HealthController.java
@@ -7,15 +7,15 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
 
     @RequestMapping(value = "/health", method = {RequestMethod.GET})
-    @ApiOperation(value = "health", nickname = "health")
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success", response = String.class),
-            @ApiResponse(code = 500, message = "Internal Server Error")})
-
+////    @ApiOperation(value = "health", nickname = "health")
+//    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success", response = String.class),
+//            @ApiResponse(code = 500, message = "Internal Server Error")})
     public String index() {
         return "OK";
     }
